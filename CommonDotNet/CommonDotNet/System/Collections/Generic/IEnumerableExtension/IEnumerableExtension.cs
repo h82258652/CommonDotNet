ffad0e9ad273cc4570f3ed56d47a8cb50a40e146
h82258652 <842053625@qq.com>
2014-07-14 21:33:57 +0800

    add missing files and reformat code
    rt

--- CommonDotNet/CommonDotNet/System/Collections/Generic/IEnumerableExtension/IEnumerableExtension.cs
+++ CommonDotNet/CommonDotNet/System/Collections/Generic/IEnumerableExtension/IEnumerableExtension.cs
@@ -1,22 +1,11 @@
 ﻿using System.Linq;
 
-
-
 namespace System.Collections.Generic
-
 {
-
     /// <summary>
-
     /// IEnumerable 扩展类。
-
     /// </summary>
-
     public static partial class IEnumerableExtension
-
     {
-
-
     }
-
 }

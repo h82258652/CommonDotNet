2816ddf8e0ab2e8d7d5b9405689a3c5e283b9ee3
h82258652 <842053625@qq.com>
2014-08-06 15:53:51 +0800

    扩展marshal类。

---
+++ CommonDotNet/CommonDotNet/System/Runtime/InteropServices/MarshalExtensions.cs
@@ -0,0 +1,32 @@
+﻿// ReSharper disable CheckNamespace
+namespace System.Runtime.InteropServices
+// ReSharper restore CheckNamespace
+{
+    /// <summary>
+    /// Marshal 扩展类。
+    /// </summary>
+    public static class MarshalExtensions
+    {
+        /// <summary>
+        /// 返回非托管类型的大小（以字节为单位）。
+        /// </summary>
+        /// <typeparam name="T">要返回其大小的类型。</typeparam>
+        /// <returns><c>T</c> 泛型类型参数指定的类型的大小（以字节为单位）。</returns>
+        public static int SizeOf<T>()
+        {
+            return Marshal.SizeOf(typeof(T));
+        }
+
+        /// <summary>
+        /// 返回指定类型的对象的非托管大小（以字节为单位）。
+        /// </summary>
+        /// <typeparam name="T">要返回其大小的对象。</typeparam>
+        /// <param name="structure">structure 参数的类型。</param>
+        /// <returns>非托管代码中指定对象的大小（以字节为单位）。</returns>
+        /// <exception cref="ArgumentNullException"><c>structure</c> 参数为 null。</exception>
+        public static int SizeOf<T>(T structure)
+        {
+            return Marshal.SizeOf(structure);
+        }
+    }
+}
